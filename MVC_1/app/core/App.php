8a4ea29a2876668eab607c08528afa29ac4b0600
MKA1500 <maciejkabsch@gmail.com>
2019-07-18 21:13:30 +0200

    htaccess

--- MVC_1/app/core/App.php
+++ MVC_1/app/core/App.php
@@ -6,12 +6,12 @@ class App {
   protected $params = [];
 
   public function __construct() {
-    $this->parseUrl();
+    $url = $this->parseUrl();
   }
 
   public function parseUrl() {
     if(isset($_GET['url'])) {
-      echo $_GET['url'];
+      return $url = explode('/', filter_var(rtrim($_GET['url'], '/'), FILTER_SANITIZE_URL));
     }
   }
 }
